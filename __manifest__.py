476989007901d599493a1be3eaa3dc9afe2a649d
NicholasYew <nicholasyew.2023@scis.smu.edu.sg>
2026-03-26 13:43:54 +0800

    Start

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Todo List Management',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for managing internal tasks',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
